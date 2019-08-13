b43a4f17ed224f85febb2d4201937f07a5497105
EquipoConPerseverancia <ajflores.290992@gmail.com>
2019-08-13 11:42:32 -0600

    version 1

---
+++ mapa/inicio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MI MAPA</title>
+
+<script type="text/javascript">
+
+function abrirVentana(){
+        var url = "googlemap.php";
+        window.open(url, "Nuevo","alwaysRaised=no,toolbar=no,menubar=no,status=no,resizable=no,width=400,height=300,location=no");
+      }
+
+	</script>
+
+</head>
+<body>
+	<center>
+ <input type="button" value="MAPA"onclick="abrirVentana();">
+ </center>
+
+</body>
+</html>
